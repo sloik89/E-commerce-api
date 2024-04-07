1ba4e10ed13740675b65d123eb4f3127d9d064ee
sloik89 <sloik89@gmail.com>
2024-04-07 09:26:24 +0200

    complete getSingleProduct

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,12 +1,25 @@
 import Product from "../models/Product.js";
+import { StatusCodes } from "http-status-codes";
+import { NotFound } from "../errors/index.js";
 const createProduct = async (req, res) => {
-  res.send("createProduct");
+  req.body.user = req.user._id;
+  const product = await Product.create(req.body);
+  console.log(product);
+
+  res.status(StatusCodes.OK).json({ msg: "product created" });
 };
 const getAllProducts = async (req, res) => {
-  res.send("getAllProducts");
+  const product = await Product.find({});
+  res.status(StatusCodes.OK).json({ products: product });
 };
 const getSingleProduct = async (req, res) => {
-  res.send("getSingleProduct");
+  console.log("get single");
+  const { id: productId } = req.params;
+  console.log(productId);
+  const product = await Product.findOne({ _id: productId });
+  console.log(product);
+
+  res.status(StatusCodes.OK).json(product);
 };
 const updateProduct = async (req, res) => {
   res.send("updateProduct");
