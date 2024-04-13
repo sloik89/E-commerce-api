b5a5fd3bc9a3e894c962276e90348bc4b92fb282
sloik89 <sloik89@gmail.com>
2024-04-14 00:58:08 +0200

    x

--- utilis/jswt.js
+++ utilis/jswt.js
@@ -18,6 +18,7 @@ const attachCokkieToResponse = (res, userToken) => {
     expires: new Date(Date.now() + oneDay),
     secure: false,
     signed: true,
+    sameSite: "none",
   });
 };
 export { createJWT, isTokenValid, attachCokkieToResponse };
