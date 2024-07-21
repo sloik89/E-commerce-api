b7db9fd3bb63199748325c9eb9cdef55baa09407
sloik89 <sloik89@gmail.com>
2024-07-21 15:25:31 +0200

    updated orders

--- controllers/ordersController.js
+++ controllers/ordersController.js
@@ -24,12 +24,37 @@ const getSingleOrder = async (req, res) => {
 };
 const getCurrentUserOrder = async (req, res) => {
   const { _id } = req.user;
-  const order = await Order.find({ user: _id });
+  const countSearchedProducts = await Order.find({
+    user: _id,
+  }).countDocuments();
+
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 3;
+  const skip = (page - 1) * limit;
+
+  const pages = Math.ceil(countSearchedProducts / limit);
+  /* page 1 limit 3
+      skip 0 * 3 = 0
+      pages  7 / 3 = 3.5 = 3
+      const order = await Order.find({ user: _id }).skip(0).limit(3)
+    page 2 limit 3
+      skip 1 * 3 = 3
+      pages  7 / 3 = 3.5 = 3
+      const order = await Order.find({ user: _id }).skip(3).limit(3)
+    page 3 limit 3
+      skip 2 * 3 = 6
+      pages  7 / 3 = 3.5 = 3
+      const order = await Order.find({ user: _id }).skip(6).limit(3)
+  */
+  const order = await Order.find({ user: _id }).skip(skip).limit(limit);
   if (!order) {
     throw new NotFound(`No orders found`);
   }
 
-  res.status(StatusCodes.OK).json(order);
+  res.status(StatusCodes.OK).json({
+    order,
+    meta: { pagination: { total: order.length, page, pages } },
+  });
 };
 const createOrder = async (req, res) => {
   const { cartItems, tax, shippingFee } = req.body;
